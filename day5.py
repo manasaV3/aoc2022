c8289006edbbbc698efbdbd7d22ff0d679709971
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2022-12-05 17:54:37 -0800

    Day 5 Cleanup

--- day5.py
+++ day5.py
@@ -1,23 +1,25 @@
 import re
 from copy import deepcopy
 
-input_file = open('input-5-1.txt')
+def get_top_cargos(arrangement):
+  return ''.join([arrangement[i][-1] for i in range(0, len(arrangement))])
+
 
 is_drawing = True
 drawing_input = []
 drawing2 = drawing1 = []
 
-for line in input_file:
+for line in open('input-5-1.txt'):
   clean_line = line.replace('\n', '')
 
   if is_drawing:    
     if len(clean_line) > 0:
       arrangement = [ clean_line[i * 4 + 1] for i in range(0, int((len(clean_line) + 1) /4)) ]
       drawing_input.append(arrangement)      
-    else:      
+    else:
       for i in range(0, len(drawing_input[0])):
         stack = []
-        start = len(drawing_input) - 1
+        start = len(drawing_input) - 2
         while start >= 0 and drawing_input[start][i] != ' ':
           stack.append(drawing_input[start][i])
           start -= 1
@@ -27,17 +29,18 @@ for line in input_file:
       is_drawing = False
 
   else:
-    rearrangement_regex = re.match('move (\d*) from (\d*) to (\d*)', clean_line)       
+    rearrangement_regex = re.match('^move (\d*) from (\d*) to (\d*)$', clean_line)
     match = rearrangement_regex.groups()
 
     count = int(match[0])
     source = int(match[1]) - 1
     dest = int(match[2]) - 1
 
-    for i in drawing2[source][-1 * count:]:
-      drawing2[dest].append(i)
+    for i in range(0, count):
       drawing1[dest].append(drawing1[source].pop())
-      drawing2[source].pop()
+    
+    drawing2[dest].extend(drawing2[source][-count:])
+    del drawing2[source][-count:]
 
-print(''.join([drawing1[i][-1] for i in range(0, len(drawing1))]))
-print(''.join([drawing2[i][-1] for i in range(0, len(drawing2))]))
+print(f'moving single crate solution: {get_top_cargos(drawing1)}')
+print(f'moving multiple crate solution: {get_top_cargos(drawing2)}')
